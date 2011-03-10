44851966f778ae8b4c41b04d4af02e25c54b39d7
Sebastian Arcila Valenzuela <sebastianarcila@gmail.com>
2011-03-10 10:21:05 -0500

    fix move_to is a class method no instance method

--- lib/heroku/bartender/command.rb
+++ lib/heroku/bartender/command.rb
@@ -3,7 +3,7 @@ module Heroku
     class Command
       # move to an specific commit
       # Pending ada more testing stuff
-      def move_to release, heroku_remote
+      def self.move_to release, heroku_remote
         `git push -f #{heroku_remote} #{release}:master`
       end
     end
