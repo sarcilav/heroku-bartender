1803ff9e2a63311d1a822a88feadda4b86d0f679
Sebastian Arcila Valenzuela <sebastianarcila@gmail.com>
2011-03-11 14:47:07 -0500

    add tests and sha_exist? to command.rb

--- lib/heroku/bartender/command.rb
+++ lib/heroku/bartender/command.rb
@@ -1,10 +1,21 @@
+require 'grit'
 module Heroku
   module Bartender
     class Command
+      def self.sha_exist?(sha)
+        unless `git show #{sha}`.empty?
+          return true
+        end
+        false
+      end
       # move to an specific commit
-      # Pending ada more testing stuff
       def self.move_to release, heroku_remote
-        `git push -f #{heroku_remote} #{release}:master`
+        repo = Grit::Repo.new('.') 
+        if repo.remote_list.include? heroku_remote and sha_exist? release
+          `git push -f #{heroku_remote} #{release}:master`
+          return true
+        end
+        false
       end
     end
   end
--- spec/command_spec.rb
+++ spec/command_spec.rb
@@ -1,12 +1,43 @@
 require "spec_helper"
 
 describe Heroku::Bartender::Command do
+  
   describe "Methods" do
-    it "should respond_to move_to" do
+    it "should respond to move_to" do
       Heroku::Bartender::Command.respond_to?(:move_to).should be_true
     end
     describe "move_to" do
-      it "need to be tested"
+      before(:each) do
+        @good_sha_commit =  Heroku::Bartender::Log.get_log.first.sha
+        @good_remote = Grit::Repo.new('.').remote_list.last
+      end
+      context "remote and release exist" do
+        it "should return true" do
+          Heroku::Bartender::Command.move_to(@good_sha_commit, @good_remote).
+            should be_true
+        end
+      end
+      context "one of the params are wrong" do
+        it "should return false" do
+          Heroku::Bartender::Command.move_to(@good_sha_commit, "production123").
+            should be_false
+          Heroku::Bartender::Command.move_to("0", @good_remote).should be_false
+          Heroku::Bartender::Command.move_to("0", "production123").should be_false
+        end
+
+      end
+    end
+  end
+  it "should respond to exist sha" do
+    Heroku::Bartender::Command.respond_to?(:sha_exist?).should be_true
+  end
+  describe "sha_exist?" do
+    it "should return true with an existing commit" do
+      Heroku::Bartender::Command.
+        sha_exist?(Heroku::Bartender::Log.get_log.first.sha).should be_true
+    end
+    it "should return false with an unexisting commit" do
+      Heroku::Bartender::Command.sha_exist?('0').should be_false
     end
   end
 end
