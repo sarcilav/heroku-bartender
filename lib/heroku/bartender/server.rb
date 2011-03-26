d43f8667705a710d28d8a22ec18eb33ea996208e
Federico Builes <federico.builes@gmail.com>
2011-03-26 10:27:06 -0500

    Refactoring the Sinatra server.

--- lib/heroku/bartender/server.rb
+++ lib/heroku/bartender/server.rb
@@ -1,78 +1,89 @@
 require 'sinatra/base'
 require 'erb'
+
 module Heroku
   module Bartender
     class Server < Sinatra::Base
-      @@heroku_remote
-      @@user
-      @@pass
-      @@deployed_versions = {}
-      @@status = nil
-      dir = File.dirname(File.expand_path(__FILE__))
-      set :views,  "#{dir}/views"
-      set :public, "#{dir}/public"
+
+      configure do
+        dir = File.dirname(File.expand_path(__FILE__))
+        set :views, "#{dir}/views"
+        set :public, "#{dir}/views"
+        set :deployed_versions, { }
+        set :remote, nil
+      end
+
+      before do
+        @commits = Log.generate_commits
+        @current_version = Command.current_version(remote)
+        @current_state = html_status
+      end
 
       get "/" do
-        erb(:template, {}, :commits => Log.generate_commits,
-            :current_version => Command.current_version(@@heroku_remote),
-            :deployed_versions => @@deployed_versions,
-            :status => @@status
-            )
+        erb :template
       end
 
       post "/" do
         if params[:sha]
-          @@status = Command.move_to params[:sha], @@heroku_remote
-          @@deployed_versions[params[:sha]] = [Time.now, @@status]
+          Server.update_build_status(Command.move_to(params[:sha], remote))
+          deployed_versions[params[:sha]] = [Time.now, html_status]
         end
-        erb(:template, {}, :commits => Log.generate_commits,
-            :current_version => Command.current_version(@@heroku_remote),
-            :deployed_versions => @@deployed_versions,
-            :status => @@status
-            )
+        erb(:template)
       end
 
-      def self.start(host, port, heroku_remote, user, pass)
-        @@heroku_remote = heroku_remote
-        authorize(user, pass)
+      def self.start(host, port, heroku_remote, username, pass)
+        set :remote, heroku_remote
+        update_build_status(nil)
+        authorize(username, pass)
         Heroku::Bartender::Server.run!(:host => host, :port => port)
       end
 
       def self.authorize(user, pass)
         if user != '' && pass != ''
-          use Rack::Auth::Basic, "Restricted Area" do |username, password|
-            [username, password] == [user, pass]
+          use Rack::Auth::Basic, "Restricted Area" do |usr, pwd|
+            [usr, pwd] == [user, pass]
           end
         end
+      end
+
+      # encapsulate this behavior here so it's easier to get rid
+      # of the class variable in the future.
+      def self.update_build_status(value)
+        @@status = value
       end
 
       helpers do
         include Rack::Utils
 
+        def deployed_versions
+          settings.deployed_versions
+        end
+
+        def status
+          @@status
+        end
+
+        def remote
+          settings.remote
+        end
+
         def current_class(current_version_sha, sha)
           sha == current_version_sha ? 'current' : ''
         end
 
-        def color_status(version_sha)
-          if @@deployed_versions[version_sha]
-            status = @@deployed_versions[version_sha][1]
-            if status == true
-              return 'green'
-            elsif status == false
-              return 'red'
-            end
-            return 'yellow'
+        def html_status(state=status)
+          case state
+          when true
+            return "ok"
+          when false
+            return "fail"
+          else
+            return "unknown"
           end
-          ''
         end
 
-        def state(status)
-          if status == true
-            return 'OK'
-          elsif status == false
-            return 'FAIL'
-          end
-          'UNKNOWN'
+        def color_status(version_sha)
+          deployed_versions[version_sha].last if deployed_versions[version_sha]
         end
       end
     end
