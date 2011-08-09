c23ca78134cb472c6dfa3cbf9e85f678c29471f0
Sebastian Arcila Valenzuela <sebastianarcila@gmail.com>
2011-08-09 12:22:00 -0500

    forgot to add the log class :$

--- lib/heroku/bartender/log.rb
+++ lib/heroku/bartender/log.rb
@@ -2,23 +2,16 @@ require "grit"
 module Heroku
   module Bartender
     class Log
-      def self.get_log(options = {})
-        options[:page] ||= 0
-        offset = options[:max_per_page].to_i * options[:page].to_i
-        Grit::Repo.new('.').commits('master',
-                                    options[:max_per_page],
-                                    offset
-                                    )
-      end
-      def self.generate_commits(options = {})
-        get_log(options).map do |item|
-          Commit.new({ :sha   => item.sha,
-                       :author  => item.author.name,
-                       :email => item.author.email,
-                       :message => item.message,
-                       :date  => item.date
-                     })
+      class << self
+        def get_log(options = {})
+          options[:page] ||= 0
+          offset = options[:max_per_page].to_i * options[:page].to_i
+          Grit::Repo.new('.').commits('master',
+                                      options[:max_per_page],
+                                      offset
+                                      )
         end
+        alias :generate_commits :get_log
       end
     end
   end
